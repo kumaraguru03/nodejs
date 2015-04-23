e2b85300a48b0633bb6caa5ee8b4adf5d9120c8b
kumaraguru03 <kumaraguru03@gmail.com>
2015-04-23 01:13:18 -0700

    Create index.js

---
+++ proxy-server/index.js
@@ -0,0 +1,37 @@
+let http = require('http')
+let request = require('request')
+let fs = require('fs')
+let argv = require('yargs')
+    .default('host', '127.0.0.1')
+    .argv
+let scheme = 'http://'    
+let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
+let destinationUrl = argv.url || scheme  + argv.host + ':' + port
+let outputStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
+
+http.createServer((req, res) => {
+for (let header in req.headers) {
+    res.setHeader(header, req.headers[header])
+}   
+ outputStream.write('\n\n\n' + JSON.stringify(req.headers))
+ req.pipe(outputStream)
+ req.pipe(res)
+}).listen(8000)
+
+http.createServer((req, res) => {
+	let url = destinationUrl
+	if (req.headers['x-destination-url']) {
+		url = req.headers['x-destination-url']
+	}
+	let options = {
+		headers: req.headers,
+		url: url + req.url
+	}
+
+	outputStream.write(JSON.stringify(req.headers))
+	req.pipe(outputStream)
+
+	let destinationResponse = req.pipe(request(options))
+	destinationResponse.pipe(res)
+
+}).listen(8001)
